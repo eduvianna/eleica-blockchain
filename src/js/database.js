57b15d9f550cb849ff104e7d4f98c93d670dfec4
gabsgear <gabriel_ghellere@hotmail.com>
2018-11-07 15:19:39 -0200

    crud finish

--- src/js/database.js
+++ src/js/database.js
@@ -7,7 +7,7 @@ var knex = require('knex')({
 });
 
 
-function select() {
+function selectAll() {
     let selectResult
     selectResult = knex.select("*").from("Candidates")
     selectResult.then(function (rows) {
@@ -15,11 +15,38 @@ function select() {
     })
 }
 
-function insert() {
-    let insert = knex("Candidates").insert({ Id: 1, Name: 'Carl', Votes: 22, Partie: 'asd' })
+function selectCandidate(candidate){
+    let selectResult
+    selectResult = knex.select("*").where({Name:candidate}).from("Candidates")
+    selectResult.then(function (rows) {
+        console.log(rows)
+    })
+}
+
+function insert(candidate, partie, id, votes) {
+    let insert = knex("Candidates").insert({ Id: id, Name: candidate, Votes: votes, Partie: partie })
         .then(function (id) {
-            console.log ('insert ok')
+            console.log ('inserted')
         })
 }
-insert()
-select()
+
+function update(candidate, votes){
+    let update = knex("Candidates").where({ Name:candidate }).update({ Votes: votes })
+    .then(function (id) {
+        console.log ('updated')
+    })
+}
+
+function deleteCandidate(candidate){
+    let deleteCandidate
+    deleteCandidate = knex.delete("*").where({Name:candidate}).from("Candidates")
+    .then(function (id) {
+        console.log ('deleted')
+    })
+}
+
+//insert("Ronaldo", "Brasil", 9,0)
+//selectAll()
+//update("Ronaldo", 9)
+//selectCandidate("Ronaldo")
+//deleteCandidate("Ronaldo")
